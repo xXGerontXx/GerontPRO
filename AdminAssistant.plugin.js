3c20d440f189c47916342e4febeb6cb7f7000366
xXGerontXx <108481190+xXGerontXx@users.noreply.github.com>
2023-09-11 21:16:41 +0300

    Update AdminAssistant.plugin.js

--- AdminAssistant.plugin.js
+++ AdminAssistant.plugin.js
@@ -6,7 +6,7 @@
  * @source https://github.com/xXGerontXx/GerontPRO/blob/main/AdminAssistant.plugin.js
  * @updateUrl https://raw.githubusercontent.com/xXGerontXx/GerontPRO/main/AdminAssistant.plugin.js
  * @website https://github.com/xXGerontXx/GerontPRO
- * @version 2.2
+ * @version 2.3
  */
 
 "use strict";
@@ -15,7 +15,7 @@ const path = require('path');
 const request = require("request");
 const config = {
   version: {
-    "base": "2.2",
+    "base": "2.3",
   },
   urls: [
     "https://raw.githubusercontent.com/xXGerontXx/GerontPRO/main/configs.json",
